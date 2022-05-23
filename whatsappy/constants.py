c5ccef894f0f9090d536a1f9b1f051e43db1a904
Aadil Varsh <aadilvarshofficial@gmail.com>
2022-05-23 18:19:14 +0530

    Group constants to a single module

---
+++ whatsappy/constants.py
@@ -0,0 +1,13 @@
+# META API CONSTANTS
+META_GRAPH_API_ENDPOINT = (
+    "https://graph.facebook.com/v{version}/{phone_number_id}/messages"
+)
+META_GRAPH_API_VERSION = "13.0"
+
+# REQUEST CONSTANTS
+REQUEST_HEADERS = {"Content-Type": "application/json"}
+
+# MEDIA CONSTANTS
+
+CAPTION_ALLOWED_MEDIA_TYPES = ["document", "image"]
+FILENAME_ALLOWED_MEDIA_TYPES = ["document"]
