ade04378e22954a60d604f7ec565d03738ce7f49
Aadil Varsh <aadilvarshofficial@gmail.com>
2022-05-23 18:21:10 +0530

    Handle individual entities

--- whatsappy/__init__.py
+++ whatsappy/__init__.py
@@ -1,53 +1,39 @@
 import requests as rq
 
-from whatsapy.api_objects import *
-from whatsapy.body_generator import BodyGenerator
-from whatsapy.errors import ErrorResponse, ErrorMediaResponse
+from whatsappy.ext.message import *
+from whatsappy.schema_generator import SchemaGenerator
+from whatsappy.errors import ErrorResponse
+from whatsappy.constants import (
+    META_GRAPH_API_ENDPOINT,
+    META_GRAPH_API_VERSION,
+    REQUEST_HEADERS,
+)
 
 
-class Whatsapy:
+class Whatsappy:
     """
     Whatsapp Cloud API main wrapper class.
     """
 
     def __init__(self, access_token: str, phone_number_id: str) -> None:
-        self.body_generator = BodyGenerator()
+        self.schema_generator = SchemaGenerator()
         self.access_token = access_token
-
         self.phone_number_id = phone_number_id
 
-        self.url = f"https://graph.facebook.com/v13.0/{self.phone_number_id}/messages"
+        self.url = META_GRAPH_API_ENDPOINT.format(
+            version=META_GRAPH_API_VERSION, phone_number_id=self.phone_number_id
+        )
 
-    def push(self, message: Message):
-        body = self.body_generator.generate(message, dump_json_str=True)
-        headers = {
-            "Authorization": f"Bearer {self.access_token}",
-            "Content-Type": "application/json",
-        }
+    def send(self, message: Message):
+        body = self.schema_generator.generate(message, dump_json_str=True)
+        headers = REQUEST_HEADERS
+        headers["Authorization"] = f"Bearer {self.access_token}"
 
-        if message.media_order == "before":
-            self.push_media(message)
-            response = rq.post(self.url, headers=headers, data=body)
+        print(body)
 
-        elif message.media_order == "after":
-            response = rq.post(self.url, headers=headers, data=body)
-            self.push_media(message)
+        response = rq.post(self.url, headers=headers, data=body)
 
         if response.status_code == 200:
             return True
 
         raise ErrorResponse(response)
-
-    def push_media(self, message: Message):
-        body = self.body_generator.generate_media_body(message)
-        headers = {
-            "Authorization": f"Bearer {self.access_token}",
-            "Content-Type": "application/json",
-        }
-
-        response = rq.post(self.url, data=body, headers=headers)
-
-        if response.status_code == 200:
-            return True
-
-        raise ErrorMediaResponse(response)
