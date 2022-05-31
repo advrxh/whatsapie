3461118a825de6e8b5a5ff070af0dce18b2245e0
Aadil Varsh <aadilvarshofficial@gmail.com>
2022-05-31 21:12:56 +0530

    I hate the name Whatsappy

--- whatsappy/ext/__init__.py
+++
@@ -1,1 +0,0 @@
-from whatsappy.ext.message import *
--- whatsappy/ext/message/__init__.py
+++
@@ -1,4 +0,0 @@
-from whatsappy.ext.message.message import Message
-from whatsappy.ext.message.location import LocationMessage
-from whatsappy.ext.message.text import TextMessage
-from whatsappy.ext.message.media import MediaMessage
